3151902735dc9752e71aaadc454a4443688d2904
Kamron or Камрон <kamrush6429@gmail.com>
2023-07-24 13:07:14 +0500

    reset function

--- function.ts
+++ function.ts
@@ -15,3 +15,16 @@ const getChannelName:TypeChannelFunction = (name) => {
 const getNumbers = (...numbers: number[]) => {
   return numbers
 }
+
+// перезагрузки 
+
+function getCar(name:string):string
+function getCar(name:string, price:number):string
+
+function getCar(name:string, price?:number):string {
+  return price ? `Name: ${name}, Price: ${price}` : `Name: ${name}`
+}
+
+const car1 = getCar('BMW')
+const car2 = getCar('BMW', 10000)
+// const car2 = getCar('BMW', 10000, 'dfsfsd')  --error
